d3248b3302434b06fac7d79653c671bc0c07ed9e
Yuki Imai <yuki08281993@gmail.com>
2019-11-13 23:40:28 +0900

    infer関数を定義

--- test/SimpleTypingSpec.hs
+++ test/SimpleTypingSpec.hs
@@ -2,15 +2,48 @@ module SimpleTypingSpec (spec) where
 
 import Test.Hspec
 
+import SimpleType
+import SimpleTyping
+import Expr
+
 
 spec :: Spec
 spec = do
+    inferSpec
     extractSpec
     unifySpec
     ftvSpec
     substituteOneSpec
     substituteSpec
     substituteEqsSpec
+
+inferSpec :: Spec -- 散発的なテストにとどめて、全体の仕組みが動いているかどうかのチェックだけをする。
+inferSpec =
+    describe "infer" $ do
+        context "given typable expressions" $ do
+            it "types 1" $
+                EInt 1 `hasType` TInt
+            it "types 1 + 2" $
+                EAdd (EInt 1) (EInt 2) `hasType` TInt
+            it "types (fun x -> x + 1)" $
+                EAbs "x" (EAdd (EVar "x") (EInt 1)) `hasType` TFun TInt TInt
+            it "types (fun x -> x + 1) 1" $
+                EApp (EAbs "x" (EAdd (EVar "x") (EInt 1))) (EInt 1) `hasType` TInt
+        context "given untypable expressions" $ do
+            it "rejects x" $
+                hasNoType $ EVar "x"
+            it "rejects 1 + true" $
+                hasNoType $ EAdd (EInt 1) (EBool True)
+            it "rejects (fun x -> x + 1) true" $
+                hasNoType $ EApp (EAbs "x" (EAdd (EVar "x") (EInt 1))) (EBool True)
+
+
+
+hasType :: Expr -> SimpleType -> Expectation
+e `hasType` t = infer e `shouldBe` Just t
+
+hasNoType :: Expr -> Expectation
+hasNoType e = infer e `shouldBe` Nothing
 
 extractSpec :: Spec
 extractSpec =
